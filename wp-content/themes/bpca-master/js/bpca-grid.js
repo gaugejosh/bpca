de56be3da764fd66e35e95989d7befadb05def01
Joshua Romero <josh@gaugeinteractive.com>
2015-02-24 00:55:02 -0500

    Fixed the slider toggle issue and slowed the slide speed down a bit.

--- wp-content/themes/bpca-master/js/bpca-grid.js
+++ wp-content/themes/bpca-master/js/bpca-grid.js
@@ -16,27 +16,35 @@ $(document).ready(function () {
         // copy description and append it to container
         var $content = $(this).find('.grid-description').html();
 
-        // revert row border color
-        $grid.prev().css('border-bottom', '#ebedee');
-        $grid.css('border-bottom', '#ebedee');
-        $grid.next().css('border-bottom', '#ebedee');
+        if ($(this).hasClass('active')) {
+            // revert row border color
+            $grid.css('border-bottom', '#ebedee');
 
-        // remove any active classes
-        $grid.removeClass('active');
+            // remove any active classes
+            $grid.removeClass('active');
 
-        // hide any open descriptions
-        $('.grid-alt').slideUp();
+            // hide any open descriptions
+            $('.grid-alt').slideUp('slow');
+        } else {
+            // revert row border color
+            $grid.css('border-bottom', '#ebedee');
 
-        // add the active class to the current element
-        $(this).addClass('active');
+            // remove any active classes
+            $grid.removeClass('active');
 
-        $grid.prev().css('border-bottom', '#19a6e2');
-        $grid.css('border-bottom', '#19a6e2');
-        $grid.next().css('border-bottom', '#19a6e2');
+            // hide any open descriptions
+            $('.grid-alt').slideUp();
 
-        // show current element description
-        $target.html($content).slideDown();
+            // add the active class to the current element
+            $(this).addClass('active');
 
+            $grid.prev().css('border-bottom', '#19a6e2');
+            $grid.css('border-bottom', '#19a6e2');
+            $grid.next().css('border-bottom', '#19a6e2');
+
+            // show current element description
+            $target.html($content).slideDown('slow');
+        }
     });
 
 });
