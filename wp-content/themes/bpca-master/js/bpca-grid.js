7590e1fb349102aeeb9d95fb6142890e8f3f64c8
Joshua Romero <josh@gaugeinteractive.com>
2015-02-24 00:46:23 -0500

    Addressing the bottom border on grids issue.

--- wp-content/themes/bpca-master/js/bpca-grid.js
+++ wp-content/themes/bpca-master/js/bpca-grid.js
@@ -16,6 +16,11 @@ $(document).ready(function () {
         // copy description and append it to container
         var $content = $(this).find('.grid-description').html();
 
+        // revert row border color
+        $grid.prev().css('border-bottom', '#ebedee');
+        $grid.css('border-bottom', '#ebedee');
+        $grid.next().css('border-bottom', '#ebedee');
+
         // remove any active classes
         $grid.removeClass('active');
 
@@ -24,6 +29,10 @@ $(document).ready(function () {
 
         // add the active class to the current element
         $(this).addClass('active');
+
+        $grid.prev().css('border-bottom', '#19a6e2');
+        $grid.css('border-bottom', '#19a6e2');
+        $grid.next().css('border-bottom', '#19a6e2');
 
         // show current element description
         $target.html($content).slideDown();
